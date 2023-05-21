4bfd21cb72ab857181a9419f18352a98886d49dd
CesarAugusto <pechitosoltero@gmail.com>
2023-05-21 15:11:13 -0500

    Primer Cambio

---
+++ pw_u1_p4_t_as/src/test/java/com/cursospringboot/app/PwU1P4TAsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cursospringboot.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PwU1P4TAsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
